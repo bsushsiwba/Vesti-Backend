46b611fa94295e0aa40880274c9a362c6e99597f
bsushsiwba <66201863+bsushsiwba@users.noreply.github.com>
2025-05-31 13:17:06 +0500

    reverted sam commands

--- sam.sh
+++ sam.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/bsushsiwba/vesti-backend-sam && cd vesti-backend-sam && python -m venv sam && source sam/bin/activate && python main.py
+git clone https://github.com/bsushsiwba/vesti-backend-sam && cd vesti-backend-sam && python -m venv sam && source sam/bin/activate && pip install torch==2.5.1 && pip install packaging hydra-core scikit-learn wheel iopath onnxruntime rembg && pip install transformers==4.45.2 && pip install git+https://github.com/bsushsiwba/grounded-sam-pypi.git && python main.py
