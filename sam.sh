24322acdc9e47611edfe7e6adcaf8b9214b3be9b
bsushsiwba <66201863+bsushsiwba@users.noreply.github.com>
2025-05-31 12:58:19 +0500

    updated SAM commands

--- sam.sh
+++ sam.sh
@@ -1,1 +1,24 @@
-git clone https://github.com/bsushsiwba/vesti-backend-sam && cd vesti-backend-sam && python -m venv sam && source sam/bin/activate && pip install torch==2.5.1 && pip install packaging hydra-core scikit-learn wheel iopath onnxruntime rembg && pip install transformers==4.45.2 && pip install git+https://github.com/bsushsiwba/grounded-sam-pypi.git && python main.py
+#!/bin/bash
+
+# Fail on error
+set -e
+
+# Clone repo
+git clone https://github.com/bsushsiwba/vesti-backend-sam
+cd vesti-backend-sam
+
+# Create and activate venv
+python -m venv sam
+source sam/bin/activate
+
+# Install dependencies
+pip install --upgrade pip
+pip install packaging hydra-core scikit-learn wheel iopath onnxruntime rembg
+pip install transformers==4.45.2
+pip install git+https://github.com/bsushsiwba/grounded-sam-pypi.git
+
+# Set PYTHONPATH so that local modules can be found
+export PYTHONPATH=$(pwd)
+
+# Run the script
+python main.py
